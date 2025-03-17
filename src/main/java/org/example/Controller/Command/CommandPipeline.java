ba73445df3e77ac13bfc1ead3461bfef49e2d9df
Christoffer Morales <christofferm91@hotmail.com>
2025-03-17 22:15:54 +0100

    lagt in ny class för command pipelines i en lista. fixat pojo klasserna på pants etc. lagt in en lista för observers. och lite justeringar i controllern i samband med ändringarna

--- src/main/java/org/example/Controller/Command/CommandPipeline.java
+++ src/main/java/org/example/Controller/Command/CommandPipeline.java
@@ -8,10 +8,11 @@ import java.util.List;
 public class CommandPipeline {
     private List<ClothingCommand> commands = new ArrayList<>();
 
+    //metoden för att lägga till commands i pipeline listan
     public void addCommand(ClothingCommand command) {
         commands.add(command);
     }
-
+    // metod för att utföra commandon i ordning från listan
     public void executeCommands(Clothing clothing) {
         for (ClothingCommand command : commands) {
             command.execute(clothing);
